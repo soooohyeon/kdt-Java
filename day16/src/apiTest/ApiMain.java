8651a5ae3fb4fc7b8e4615818fc8db4fe6fbe06b
soooohyeon <dyddyd310@gmail.com>
2024-12-15 18:13:21 +0900

    241215 코드 정리

--- day16/src/apiTest/ApiMain.java
+++ day16/src/apiTest/ApiMain.java
@@ -2,12 +2,12 @@ package apiTest;
 
 public class ApiMain {
 	public static void main(String[] args) {
-//		Calc c = new Calc();
-//		System.out.println(c);
-//		try {
-//			System.out.println(c.divide(10, 0));
-//		} catch (ArithmeticException e) {
-//			System.out.println("0으로 나눌 수 없습니다.");
-//		}
+		Calc c = new Calc();
+		System.out.println(c);
+		try {
+			System.out.println(c.divide(10, 0));
+		} catch (ArithmeticException e) {
+			System.out.println("0으로 나눌 수 없습니다.");
+		}
 	}
 }
--- day16/src/streamTest/StreamTest3.java
+++ day16/src/streamTest/StreamTest3.java
@@ -14,11 +14,11 @@ public class StreamTest3 {
 				.map(String::toUpperCase)			// 각 요소를 대문자로 바꿈
 				.toArray(String[]::new);			// 바꾼 요소들을 선언해둔 upperName 배열에 대입
 		
-//		System.out.println(upperName);
+		System.out.println(upperName);
 		
 //		sortName 문자열 배열을 선언 후 names2 배열을 내림차순으로 정렬한 뒤 선언해둔 sortName 문자열(객체) 배열에 값 대입
-//		String[] sortName = Arrays.stream(names2).sorted().toArray(String[]::new);
-//		Arrays.stream(sortName).forEach(System.out::print);
+		String[] sortName = Arrays.stream(names2).sorted().toArray(String[]::new);
+		Arrays.stream(sortName).forEach(System.out::print);
 		
 //		Arrays.stream(upperName).forEach(System.out::println);	// upperName 배열의 요소마다 줄바꿈 후 전체 출력
 				
