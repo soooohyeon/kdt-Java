07e23a7bb14846652a0df46d73c2315cdccd4907
soooohyeon <dyddyd310@gmail.com>
2024-12-15 23:04:02 +0900

    241215 코드 정리 커밋

--- day07/src/task/ArrayTask01.java
+++ day07/src/task/ArrayTask01.java
@@ -1,7 +1,10 @@
 //	박수현
 package task;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ArrayTask01 {
 	public static void main(String[] args) {
@@ -18,13 +21,13 @@ public class ArrayTask01 {
 //		4) for (i = 0; i < 배열의 길이; i++) {
 //		5)	배열명[i] 출력 }
 		
-		int ar1[] = new int [100];
-		for (int i = 0; i < ar1.length; i++) {
-			ar1[i] = ar1.length - i;
-		}
-		for (int i = 0; i < ar1.length; i++) {
-			System.out.println(ar1[i]);
-		}
+//		int ar1[] = new int [100];
+//		for (int i = 0; i < ar1.length; i++) {
+//			ar1[i] = ar1.length - i;
+//		}
+//		for (int i = 0; i < ar1.length; i++) {
+//			System.out.println(ar1[i]);
+//		}
 		
 //		2. 1 ~ 10까지의 값을 배열에 넣고 총 합 구하기
 //			일반 for문만 사용
@@ -60,20 +63,20 @@ public class ArrayTask01 {
 //		8) 결과출력
 //		9) 버퍼 비우기
 
-		Scanner sc = new Scanner(System.in);
-		int ar3[] = new int[4];
-		int total2 = 0;
-		double avg = 0.0;
-		System.out.print("정수 4개 입력 (띄어쓰기로 구분) : ");
-		for (int i = 0; i < ar3.length; i++) {
-			ar3[i] = sc.nextInt();
-		}
-		for(int num : ar3) {
-			total2 += num;
-		}
-		avg = (double)total2 / ar3.length;
-		System.out.printf("입력하신 숫자 4개의 평균은 %.4f 입니다.\n", avg);
-		sc.nextLine();
+//		Scanner sc = new Scanner(System.in);
+//		int ar3[] = new int[4];
+//		int total2 = 0;
+//		double avg = 0.0;
+//		System.out.print("정수 4개 입력 (띄어쓰기로 구분) : ");
+//		for (int i = 0; i < ar3.length; i++) {
+//			ar3[i] = sc.nextInt();
+//		}
+//		for(int num : ar3) {
+//			total2 += num;
+//		}
+//		avg = (double)total2 / ar3.length;
+//		System.out.printf("입력하신 숫자 4개의 평균은 %.4f 입니다.\n", avg);
+//		sc.nextLine();
 		
 //		4. 0, 1, 2, 3, 4, 0, 1, 2, 3, 4 값을 배열에 담고 출력
 //			출력시 for-each문 사용
@@ -86,17 +89,17 @@ public class ArrayTask01 {
 //		6)		배열명[i] = i - 5; } }
 //		7) foreach문 사용해서 출력
 		
-		int[] ar4 = new int[10];
-		for (int i = 0; i < ar4.length; i++) {
-			if (i < 5) {
-				ar4[i] = i;
-			} else {
-				ar4[i] = i-5;
-			}
-		}
-		for (int i : ar4) {
-			System.out.println(i);
-		}
+//		int[] ar4 = new int[10];
+//		for (int i = 0; i < ar4.length; i++) {
+//			if (i < 5) {
+//				ar4[i] = i;
+//			} else {
+//				ar4[i] = i-5;
+//			}
+//		}
+//		for (int i : ar4) {
+//			System.out.println(i);
+//		}
 		
 //		5. A ~ F까지를 C만 제외하고 배열에 담아 출력
 //			일반 for문 사용
@@ -160,26 +163,27 @@ public class ArrayTask01 {
 //			1번쨰 부터 차례로 값을 비교하며 더 큰 수를 최솟값 변수에 담기
 //		6) 구한 최솟값, 최대값 출력 
 		
-		int ar7[] = new int[5];
-		int minimum = 0, maximum = 0;
-		System.out.print("정수 5개 입력 (띄어쓰기로 구분) : ");
-		for (int i = 0; i < ar7.length; i++) {
-			ar7[i] = sc.nextInt();
-		}
-		sc.nextLine();
-		minimum = ar7[0];
-		for (int i = 1; i < ar7.length; i++) {
-			if (minimum < ar7[i]) {
-				minimum = ar7[i];
-			}
-		}
-		maximum = ar7[0];
-		for (int i = 1; i < ar7.length; i++) {
-			if (maximum > ar7[i]) {
-				maximum = ar7[i];
-			}
-		}
-		System.out.println("최솟값 : " + minimum + "\n최대값 : " + maximum);
+//		Scanner sc = new Scanner(System.in);
+//		int ar7[] = new int[5];
+//		int minimum = 0, maximum = 0;
+//		System.out.print("정수 5개 입력 (띄어쓰기로 구분) : ");
+//		for (int i = 0; i < ar7.length; i++) {
+//			ar7[i] = sc.nextInt();
+//		}
+//		sc.nextLine();
+//		minimum = ar7[0];
+//		for (int i = 1; i < ar7.length; i++) {
+//			if (minimum < ar7[i]) {
+//				minimum = ar7[i];
+//			}
+//		}
+//		maximum = ar7[0];
+//		for (int i = 1; i < ar7.length; i++) {
+//			if (maximum > ar7[i]) {
+//				maximum = ar7[i];
+//			}
+//		}
+//		System.out.println("최솟값 : " + minimum + "\n최대값 : " + maximum);
 		                     
 //		8. 사물함의 비밀번호를 설정하고 사용자에게 이용할 사물함 번호와 비밀번호를 입력받아
 //		   입력된 사물함의 비밀번호와 설정한 비밀번호를 비교하여 일치 여부 출력하기
@@ -199,42 +203,43 @@ public class ArrayTask01 {
 //		13) 	메세지(일치합니다) 출력 후 반복문 즉시 종료; } }
 //		14) 버퍼 비우기
 		
-		int password[] = new int[5];
-		int userPassword[] = new int[5];
-		int cabinet = 0, userCabinet = 0;
-		boolean isTrue = true;	// 비밀번호 일치 여부 변수
-		System.out.print("이용하시고 싶은 사물함 번호를 입력해주세요 : ");
-		cabinet = sc.nextInt();
-		System.out.print(cabinet + "번을 선택하셨습니다.\n0 ~ 9로만 5자리 비밀번호를 입력하세요. : ");
-		for (int i = 0; i < password.length; i++) {
-			password[i] = sc.nextInt();
-		}
-		System.out.print("\n이용하신 사물함 번호를 입력해주세요 : ");
-		userCabinet = sc.nextInt();
-		if (cabinet != userCabinet) {	// 사물함 번호가 틀리면 프로그램 종료
-			System.out.println("틀린 사물함 번호입니다.\n처음부터 다시 시도해주세요.");
-		} else {	// 사물함 번호가 맞다면 비밀번호 입력
-			System.out.print(userCabinet + "번 사물함 번호를 입력해주세요. : ");
-			for (int i = 0; i < userPassword.length; i++) {
-				userPassword[i] = sc.nextInt();
-			}
-			for (int i = 0; i < password.length; i++) {	// 비밀번호 일치 여부 확인
-				if (password[i] != userPassword[i]) {
-					System.out.println("불일치");
-					isTrue = false;
-				} 
-				if (isTrue == false) {
-					System.out.println("종료");
-					System.out.println("비밀번호가 틀렸습니다.");
-					break;
-				} else if (i == password.length-1 && isTrue == true) {
-					System.out.println("일치");
-					System.out.println("문이 열렸습니다.\n이용해주셔서 감사합니다..");
-					break;
-				}
-			}
-		}
-		sc.nextLine();
+//		Scanner sc = new Scanner(System.in);
+//		int password[] = new int[5];
+//		int userPassword[] = new int[5];
+//		int cabinet = 0, userCabinet = 0;
+//		boolean isTrue = true;	// 비밀번호 일치 여부 변수
+//		System.out.print("이용하시고 싶은 사물함 번호를 입력해주세요 : ");
+//		cabinet = sc.nextInt();
+//		System.out.print(cabinet + "번을 선택하셨습니다.\n0 ~ 9로만 5자리 비밀번호를 입력하세요. : ");
+//		for (int i = 0; i < password.length; i++) {
+//			password[i] = sc.nextInt();
+//		}
+//		System.out.print("\n이용하신 사물함 번호를 입력해주세요 : ");
+//		userCabinet = sc.nextInt();
+//		if (cabinet != userCabinet) {	// 사물함 번호가 틀리면 프로그램 종료
+//			System.out.println("틀린 사물함 번호입니다.\n처음부터 다시 시도해주세요.");
+//		} else {	// 사물함 번호가 맞다면 비밀번호 입력
+//			System.out.print(userCabinet + "번 사물함 번호를 입력해주세요. : ");
+//			for (int i = 0; i < userPassword.length; i++) {
+//				userPassword[i] = sc.nextInt();
+//			}
+//			for (int i = 0; i < password.length; i++) {	// 비밀번호 일치 여부 확인
+//				if (password[i] != userPassword[i]) {
+//					System.out.println("불일치");
+//					isTrue = false;
+//				} 
+//				if (isTrue == false) {
+//					System.out.println("종료");
+//					System.out.println("비밀번호가 틀렸습니다.");
+//					break;
+//				} else if (i == password.length-1 && isTrue == true) {
+//					System.out.println("일치");
+//					System.out.println("문이 열렸습니다.\n이용해주셔서 감사합니다..");
+//					break;
+//				}
+//			}
+//		}
+//		sc.nextLine();
 		
 //		9. 학생 1명의 3과목 총점과 평균(소수점 3자리까지)을 구하고 출력하기
 //			3과목은 배열에 입력받아 저장하며 for-each문만 사용 (일반 for문 사용 금지)
@@ -245,30 +250,30 @@ public class ArrayTask01 {
 //		4) 총점 형변환해서 평균 구하기, 평균 = (double)(총점) / 배열길이
 //		5) 결과 출력 (총점, 평균) - printf();
 //		6) 버퍼 비우기 (입력 클래스 종료)
-		
-		int scores[] = new int[3];
-		int index = 0;		// for-each문에서 사용하기 위한 변수
-		int total3 = 0;
-		double avg2 = 0.0;
-		
-//		점수 입력 방법 (1)
-//		System.out.print("3과목 점수 입력 (띄어쓰기로 구분) : "); 
-//		scores[0] = sc.nextInt();
-//		scores[1] = sc.nextInt();
-//		scores[2] = sc.nextInt();
-		
-//		점수 입력 방법 (2) : for-each 사용, 인덱스 변수를 따로 사용해야해서 일반 for문을 이용하는게 더 간편함
-		for (int score : scores) {
-			System.out.print("점수 입력 : "); 
-			score = sc.nextInt();
-			scores[index++] = score;	// 증감식 후위형으로 사용으로 인덱스 0부터 시작
-		}
-		
-		for (int score : scores) {	// 점수배열 반복 돌려서 총합 구하기
-			total3 += score;
-		}
-		avg2 = (double)total3 / scores.length;
-		System.out.printf("총점 : %d점, 평균 : %.3f점", total3, avg2);
-		sc.close();
+//		Scanner sc = new Scanner(System.in);
+//		int scores[] = new int[3];
+//		int index = 0;		// for-each문에서 사용하기 위한 변수
+//		int total3 = 0;
+//		double avg2 = 0.0;
+//		
+////		점수 입력 방법 (1)
+////		System.out.print("3과목 점수 입력 (띄어쓰기로 구분) : "); 
+////		scores[0] = sc.nextInt();
+////		scores[1] = sc.nextInt();
+////		scores[2] = sc.nextInt();
+//		
+////		점수 입력 방법 (2) : for-each 사용, 인덱스 변수를 따로 사용해야해서 일반 for문을 이용하는게 더 간편함
+//		for (int score : scores) {
+//			System.out.print("점수 입력 : "); 
+//			score = sc.nextInt();
+//			scores[index++] = score;	// 증감식 후위형으로 사용으로 인덱스 0부터 시작
+//		}
+//		
+//		for (int score : scores) {	// 점수배열 반복 돌려서 총합 구하기
+//			total3 += score;
+//		}
+//		avg2 = (double)total3 / scores.length;
+//		System.out.printf("총점 : %d점, 평균 : %.3f점", total3, avg2);
+//		sc.close();
 	}
 }
